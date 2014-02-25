083cec21e746fa7046c2f29e1bb7ef0b861b206d
Andy Chen <andychchen@yahoo.com>
2014-02-25 17:10:16 -0500

    add modal

--- WebContent/js/controllers.js
+++ WebContent/js/controllers.js
@@ -49,6 +49,70 @@ angular
 							// refreshData();
 							// $scope.students = dataFactory.getAllStudents();
 
+							$scope.doUpdate = function() {
+								bootbox.form({
+									title : 'User details',
+									fields : {
+										name : {
+											label : 'Name',
+											value : 'John Connor',
+											type : 'text'
+										},
+										email : {
+											label : 'E-mail',
+											type : 'email',
+											value : 'johnconnor@skynet.com'
+										},
+										type : {
+											label : 'Type',
+											type : 'select',
+											options : [ {
+												value : 1,
+												text : 'Human'
+											}, {
+												value : 2,
+												text : 'Robot'
+											} ]
+										},
+										alive : {
+											label : 'Is alive',
+											type : 'checkbox',
+											value : true
+										},
+										loves : {
+											label : 'Loves',
+											type : 'checkbox',
+											value : [ 'bike', 'mom', 'vg' ],
+											options : [ {
+												value : 'bike',
+												text : 'Motorbike'
+											}, {
+												value : 'mom',
+												text : 'His mom'
+											}, {
+												value : 'vg',
+												text : 'Video games'
+											}, {
+												value : 'kill',
+												text : 'Killing people'
+											} ]
+										},
+										passwd : {
+											label : 'Password',
+											type : 'password'
+										},
+										desc : {
+											label : 'Description',
+											type : 'textarea'
+										}
+									},
+									callback : function(values) {
+										console.log(values)
+									}
+								});
+
+							};
+
 							$scope.doDelete = function(student) {
 								bootbox
 										.confirm(
